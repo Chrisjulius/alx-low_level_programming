c0222a97ddfc66408acef7f4fe3455be81453a33
Chrisjulius <chrisogarjulius@gmail.com>
2023-02-23 20:21:09 -0800

    fix column size

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -8,7 +8,7 @@
 
 int main(void)
 {
-	printf("with proper grammar%c but the outcome is a piece of
-art%c\n", ',', ',');
-	return (0);
+printf(\
+"with proper grammar%c but the outcome is a piece of art%c\n", ',', ',');
+return (0);
 }
