84e083905d6947117d63496291fb94917bf0794c
Chrisjulius <chrisogarjulius@gmail.com>
2023-02-24 13:04:56 -0800

    fix

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -11,15 +11,14 @@
 int main(void)
 {
 	int n;
+	int last_digit;
+	char text[20];
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
-	int last_digit;
 
 	last_digit = n % 10;
-
-	char text[20];
 
 	if (last_digit > 5)
 		text = "and is greater than 5\n";
