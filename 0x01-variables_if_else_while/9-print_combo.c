1428ea1ac03a9a459f353aaec61c5d3d72641ed5
Chrisjulius <chrisogarjulius@gmail.com>
2023-02-24 20:38:23 -0800

    added activity 9

---
+++ 0x01-variables_if_else_while/9-print_combo.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+/* more headers goes there */
+/**
+* main - Entry point
+* print all combinations of single-digit numbers
+* Return: Always 0 (Success)
+*/
+/* betty style doc for function main goes there */
+int main(void)
+{
+	int x;
+
+	x = 0;
+
+	while (x < 10)
+	{
+		if (x == 9)
+		{
+			putchar(48 + x);
+			x++;
+		}
+		else
+		{
+			putchar(48 + x);
+			putchar(',');
+			putchar(' ');
+			x++;
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
+
