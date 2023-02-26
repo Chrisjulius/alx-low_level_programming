a6937174b4e272708c18e5eaa91c2e540a062614
Chrisjulius <chrisogarjulius@gmail.com>
2023-02-25 18:05:01 -0800

    complete activity 4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+  * _isalpha - entry point and confirm uppercase value
+  * @c: value to be checked
+  * Return: Always 0 (success).
+  */
+int _isalpha(int c)
+{
+	if (c > 64 && c < 91 ||c > 96 && c < 123)
+		return (1);
+	else
+		return (0);
+}
+
+
