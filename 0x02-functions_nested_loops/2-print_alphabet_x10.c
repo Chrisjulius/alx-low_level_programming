5e46a54be60894bf2320a6eb01dd78a2d2a5c661
Chrisjulius <chrisogarjulius@gmail.com>
2023-02-25 17:02:52 -0800

    complete activity 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+  * print_alphabet_x10 - entry point
+  * function that prints the alphabet
+  * Return: Always 0 (success).
+  */
+
+void print_alphabet_x10(void)
+{
+	int x, int y, char alpha;
+
+	x = 0;
+	y = 0;
+	alpha = 'a';
+
+	while (y < 10)
+	{
+		while (x < 26)
+		{
+			_putchar(alpha++);
+			x++;
+		}
+		_putchar('\n');
+		alpha = 'a';
+		x = 0;
+		y++;
+	}
+}
+
