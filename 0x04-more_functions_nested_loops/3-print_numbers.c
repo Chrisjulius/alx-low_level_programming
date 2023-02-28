1db03589d680c376fbc20cc780608dc773134e65
Chrisjulius <chrisogarjulius@gmail.com>
2023-02-28 00:49:18 -0800

    added activity 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+  * print_numbers - print the number 0 to 9
+  * 
+  *
+  * Return: void
+  */
+void print_numbers(void)
+{
+	int i, j;
+
+	j = 48;
+	
+	for(i = 0; i < 10; i++)
+		_putchar(j++);
+	_putchar('\n');
+}
