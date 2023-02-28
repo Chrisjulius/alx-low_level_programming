4bd80d1d51ad81c0963f57aabf47cd8b00f10133
Chrisjulius <chrisogarjulius@gmail.com>
2023-02-28 12:25:48 -0800

    added activity 10

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -13,11 +13,11 @@ void print_triangle(int size)
 		_putchar('\n');
 	else
 	{
-		for (a = 0; a < size; a++)
+		for (a = 1; a <= size; a++)
 		{
-			for (b = size - a; b > 1; b--)
+			for (b = size - a; b >= 1; b--)
 				_putchar(32);
-			for (c = 0; c <= 9; c++)
+			for (c = 0; c < a; c++)
 				_putchar('#');
 			_putchar('\n');
 		}
