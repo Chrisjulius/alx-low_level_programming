161f8b371b9cc7e991bf421996ce764c7ea680fc
Chrisjulius <chrisogarjulius@gmail.com>
2023-02-27 20:52:23 -0800

    added activity 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+  * add  - add two integers and return the value.
+  * @a: first operand for addition
+  * @b: second operand for addition
+  * Return: return value
+  */
+int add(int a, int b)
+{
+	int value;
+
+	value = a + b;
+	return (value);
+}
