8851870730432dbcc0dccd49bef2ad287ed1dc73
Chrisjulius <chrisogarjulius@gmail.com>
2023-02-28 05:56:42 -0800

    fix

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include "main.h"
 /**
   * main - entry point
   * prints fizzbuzz
@@ -20,9 +21,9 @@ int main(void)
 			printf("%d ", i);
 
 		if (i != 100)
-			putchar(' ');
+			printf(" ");
 	}
-	putchar('\n');
+	printf("\n");
 
 	return (0);
 }
