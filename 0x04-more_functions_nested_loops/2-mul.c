2001ff4c77873e8fc7f7f60288685d3232b877fa
Chrisjulius <chrisogarjulius@gmail.com>
2023-02-28 00:32:38 -0800

    added activity 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+  * mul - multiply two integers
+  * @a: integer no 1
+  * @b: integer no 2
+  * Return: 1 if a digit, 0 otherwise.
+  */
+int mul(int a, int b)
+{
+	int product;
+
+	product = a * b;
+	return (product);
+}
