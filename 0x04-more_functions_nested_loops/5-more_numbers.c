cf5790e1d93091a5d12ec08c109104b50934f8be
Chrisjulius <chrisogarjulius@gmail.com>
2023-02-28 04:29:52 -0800

    fix

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -7,25 +7,23 @@ i  *
   */
 void more_numbers(void)
 {
-	int i, j, a, b, num;
+	int i, j, num;
 
 	num = 48;
 	for (i = 0; i < 10; i++)
 	{
 		for (j = 0; j < 15; j++)
 		{
-			if (num > 57)
+			if (j >= 10)
 			{
-				a = 49;
-				b = num - 10;
-				_putchar(a);
-				_putchar(b);
+				putchar(49);
+				putchar(num - 10);
 				num++;
 			}
 			else
-				_putchar(num++);
+				putchar(num++);		
 		}
-		_putchar('\n');
+		putchar('\n');
 		num = 48;
 	}
 }
