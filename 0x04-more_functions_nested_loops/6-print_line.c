4b5b00efe5ad6671148b2eebb1ac3830e99d3b8a
Chrisjulius <chrisogarjulius@gmail.com>
2023-02-28 06:10:21 -0800

    added activity 6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+  * print_line - draw a straight line a number of times in the terminal
+  * @n: line repetition
+  *
+  * Return: void
+  */
+void print_line(int n)
+{
+	int i;
+
+	for (i == 0; i < n; i++)
+	{
+		if (n == 0)
+			_putchar('\n');
+		else
+			_putchar('_');
+	}
+	putchar('\n');
+}
+
