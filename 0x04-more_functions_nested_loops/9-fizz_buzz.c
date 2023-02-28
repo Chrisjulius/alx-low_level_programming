bdc8c1749981d5174b465aedfe7c7c9fa39b9e00
Chrisjulius <chrisogarjulius@gmail.com>
2023-02-28 05:11:50 -0800

    add activity 9

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+  * main - entry point
+  * prints fizzbuzz
+  * Return: always return 0 (success)
+  */
+int main(void)
+{
+	int i, count;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0)
+			printf("Fizz ");
+		else if (i % 5 == 0)
+			printf("Buzz ");
+		else if (i % 15 == 0)
+			printf("FizzBuzz ");
+		else
+			printf("%d ", i);
+	}
+	putchar('\n');
+
+	return (0);
+}
