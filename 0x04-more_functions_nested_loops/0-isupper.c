4f7371bf696167c4a922e6ee8717512f10b9df18
Chrisjulius <chrisogarjulius@gmail.com>
2023-02-27 23:07:31 -0800

    added activity 0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+  * _isupper - checks for uppercase character
+  * @c: character to check
+  * Return: 1 if uppercase, 0 otherwise.
+  */
+int _isupper(int c)
+{
+	if (c > 64 && c < 91)
+		return (1);
+	else if (c > 96 && c < 123)
+	{
+		return (0);
+	}
+	else
+		return (0);
+}
