7603cbac354f08f990bbc870fd6ea62c908b0b94
Chrisjulius <chrisogarjulius@gmail.com>
2023-03-01 05:56:08 -0800

    added activity 100

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -18,8 +18,16 @@ int main(void)
 			{
 				putchar(a);
 				putchar(b);
-				putchar(',');
-				putchar(' ');
+					if (a == '8' && b == '9')
+					{
+						a++;
+						b++;
+					}
+					else
+					{
+						putchar(',');
+						putchar(' ');
+					}
 			}
 		}
 	}
