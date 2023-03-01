3efb2c476bbfc495f8fef8acf2ddd72c65aef79d
Chrisjulius <chrisogarjulius@gmail.com>
2023-02-28 22:21:18 -0800

    complete task 101

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+  * main - print a string to standard error using unix standard
+  * Return: (1) - success
+  */
+int main(void)
+{
+	fput("and that piece of art is useful\" - Dora Korpar, 25-10-19\n");
+
+	return (1);
+}
