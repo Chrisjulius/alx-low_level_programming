efb5c517ad48f50f3d08f99aaa4d23f141eeb7cd
Chrisjulius <chrisogarjulius@gmail.com>
2023-02-28 21:28:00 -0800

    remove stderr redirection

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,11 +13,10 @@ int main(void)
         long long int d;
         float e;
 
-        fprintf(stderr, "Size of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
-        fprintf(stderr, "Size of a int: %lu byte(s)\n", (unsigned long)sizeof(b));
-        fprintf(stderr, "Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(c));
-        fprintf(stderr, "Size of a long long int: %lu byte(s) \n",
-(unsigned long)sizeof(d));
-        fprintf(stderr, "Size of a float: %lu byte(s)\n", (unsigned long)sizeof(e));
+        printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
+        printf("Size of a int: %lu byte(s)\n", (unsigned long)sizeof(b));
+        printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(c));
+        printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(d));
+        printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(e));
         return (0);
 }
