63b86957b086f46e28949d0ab8b03856f1179299
Chrisjulius <chrisogarjulius@gmail.com>
2023-03-01 09:10:30 -0800

    added activity 101

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -2,37 +2,37 @@
 /* more headers goes there */
 /**
   * main - Entry point
-  * print all combinations of two-digit numbers
+  * print all combinations of three-digit numbers
   * Return: Always 0 (Success)
   */
-/* betty style doc for function main goes there */
+/* betty style doc for function main go@es there */
 int main(void)
 {
-	int a, b;
+	int a, b, c;
 
 	for (a = '0'; a < '9'; a++)
 	{
 		for (b = a + 1; b <= '9'; b++)
 		{
-			if (b != a)
+			for (c = b + 1; c <= '9'; c++)
 			{
-				putchar(a);
-				putchar(b);
-					if (a == '8' && b == '9')
-					{
+				if ((a != b) != c)
+				{
+					putchar(a);
+					putchar(b);
+					putchar(c);
+					if (a == '7' && b == '8')
 						a++;
-						b++;
-					}
 					else
 					{
 						putchar(',');
 						putchar(' ');
 					}
+
+				}
 			}
 		}
 	}
 	putchar('\n');
-
 	return (0);
 }
-
