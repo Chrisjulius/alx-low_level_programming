76aa03935c1934cd23546671395428717998a8b3
Chrisjulius <chrisogarjulius@gmail.com>
2023-03-03 21:44:47 -0800

    added activity 102

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+/**
+  * main - starting point.
+  * Print the first 50 fibonacci numbers, start 1 and 2.
+  * Return: Always 0 (success).
+  */
+int main(void)
+{
+	int i, last, fib;
+
+	i = 0, last = 1, fib = 2;
+	while (i < 50)
+	{
+		if (i == 0)
+			printf("%d", last);
+		else if (i == 1)
+			printf(", %d", fib);
+		else
+		{
+			fib += last;
+			last = fib - last;
+			printf(", %d", fib);
+		}
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
+
