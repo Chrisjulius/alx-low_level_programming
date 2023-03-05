9f7e1b2bcc91261bd542ab0586e16d3dce2cb84b
Chrisjulius <chrisogarjulius@gmail.com>
2023-03-05 11:25:49 -0800

    added activity 101

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+  * print_number - Print integer
+  * Print integers with fuction _putchar
+  * @n: integer to print
+  * Return: void
+  */
+void print_number(int n)
+{
+	int k = n;
+
+	if (n < 0)
+	{
+		n *= -1;
+		k = n;
+		_putchar('-');
+	}
+	k /= 10;
+	if (k != 0)
+		print_number(k);
+	_putchar(n % 10 + '0');
+}
