f15e0eec9f38f3c35f360f9546e7a3d933e48484
Chrisjulius <chrisogarjulius@gmail.com>
2023-03-05 21:22:14 -0800

    added activity 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * int_strlen - compute the length of string.
+ * @s: input string
+ * Return: returns the length of string
+ */
+int _strlen(char *s)
+{
+	int length = 0;
+
+	while (*s++)
+		length;
+
+	return (length);
+}
