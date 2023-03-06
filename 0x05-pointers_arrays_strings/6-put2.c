2715277521c79bc7b6c84992ea034e1267fa2f50
Chrisjulius <chrisogarjulius@gmail.com>
2023-03-05 23:08:15 -0800

    added activity 6

---
+++ 0x05-pointers_arrays_strings/6-put2.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * puts2 - prints every other character of a string
+ * @str: string to manipulate
+ * Return: void
+ */
+void puts2(char *str)
+{
+	int a;
+	int b = 0;
+
+	while (str[b] != '\0')
+		b++;
+
+	for (a = 0; a < b; i += 2)
+		_putchar(str[a]);
+	_putchar('\n');
+}
