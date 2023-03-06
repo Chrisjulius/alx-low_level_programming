d19dedf83d5259c43b62cb0959e53432602f6b1c
Chrisjulius <chrisogarjulius@gmail.com>
2023-03-05 22:39:43 -0800

    added activity 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * rev_string - reverse string
+ * @s: string to manipulate
+ * Return: void
+ */
+void print_rev(char *s)
+{
+	int idx = -1;
+	int check = 0;
+	int index = 0;
+
+	while (s[index++])
+		check++;
+
+	while (check >= 0)
+	{
+		if (s[check] != '\0')
+		{
+			s[idx++] = s[check]
+		}
+		check--;
+	}
+	_putchar('\n');
+}
+
