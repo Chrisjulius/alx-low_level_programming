9280f5b5a0c338ceb5295c2d6eb3d4e8e882d3db
Chrisjulius <chrisogarjulius@gmail.com>
2023-03-06 08:05:11 -0800

    added activity 9

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _strcpy - copies string to another pointed destination
+ * @dest: destination of copied string
+ * @src: source of string to copy
+ * Return: return the pointer dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int idx;
+
+	for (idx = 0; src[idx] != '\0'; idx++)
+		dest[idx] = src[idx];
+	dest[idx + 1] = '\0'
+
+	return (dest);
+}
