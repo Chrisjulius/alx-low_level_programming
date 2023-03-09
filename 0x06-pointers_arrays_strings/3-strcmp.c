1ad2e866d9e37e1597102c4ca24600d0ab11b2e7
Chrisjulius <chrisogarjulius@gmail.com>
2023-03-09 08:44:01 -0800

    fix

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
-  * _strncmp - copies two string
+  * _strcmp - copies two string
   * @s1: fist string
   * @s2: second string
   * Return: return value of comparison, either less than, equal to
@@ -14,7 +14,7 @@ int _strcmp(char *s1, char *s2)
 	while (s1[a] != '\0' && s2[a] != '\0')
 	{
 		if (s1[a] != s2[a])
-			return(s1[a] - s2[a]);
+			return (s1[a] - s2[a]);
 
 		a++;
 	}
