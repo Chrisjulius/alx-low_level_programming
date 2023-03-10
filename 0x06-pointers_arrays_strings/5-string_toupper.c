a177a55d41ad0816b28dfd94b394c2323dbb5c8c
Chrisjulius <chrisogarjulius@gmail.com>
2023-03-09 19:30:44 -0800

    added activity 5

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+  * string_toupper - changes all lowercase letters of a string to uppercase
+  * @a:  pointer to a string
+  * Return: return a pointer to the resulting string
+  */
+char *string_toupper(char *a)
+{
+	int x = 0;
+
+	while (a[x] >= 'a' && a[x] <= 'z')
+	{
+		a[x] = a[x] - 32;
+		x++;
+	}
+	return (a);
+}
