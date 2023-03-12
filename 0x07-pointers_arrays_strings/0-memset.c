333e2322060db91f722e12e02d8d66221aefb8eb
Chrisjulius <chrisogarjulius@gmail.com>
2023-03-11 21:13:41 -0800

    change directory name

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+  * _memset - function to fill memory with a constant byte
+  * @s: pointer to an array which is to be filled with a byte
+  * @b: constant byte
+  * @n: number of byte to fill in s
+  * Return: returns the resulting pointer to an array
+  */
+char *_memset(char *s, char b, unsigned int n)
+{
+	int n;
+
+	for (i = 0; n > 0; i++)
+	{
+		s[i] = b;
+		n--;
+	}
+	return (s);
+}
