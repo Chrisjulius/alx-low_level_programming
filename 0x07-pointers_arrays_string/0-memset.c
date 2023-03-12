80ddcc14ee9215b0ffc7a3e18ed98635693a6c7a
Chrisjulius <chrisogarjulius@gmail.com>
2023-03-11 20:44:17 -0800

    main header file

---
+++ 0x07-pointers_arrays_string/0-memset.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+  * _memset - function to fill memory with a constant byte
+  * @s: pointer to an array which is to be filled with a byte
+  * @b: constant byte
+  * @n: number of byte to fill in *s
+  * Return: returns the resulting pointer to an array
+  */
+char *_memset(char *s, char b, unsigned int n)
+{
+	
+}
---
+++ 0x07-pointers_arrays_string/README.md
@@ -0,0 +1,3 @@
+# Even more pointers, array, and strings  
+Mor C source code for practising pointers to pointers, multi-dimensional arrays, and strings.  
+
