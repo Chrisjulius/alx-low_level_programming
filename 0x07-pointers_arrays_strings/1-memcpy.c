be188631cc2fa5610bcf2a0b990fc0e64767b45c
Chrisjulius <chrisogarjulius@gmail.com>
2023-03-11 21:37:00 -0800

    added activity 1

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -8,11 +8,12 @@
   */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	int index;
+	unsigned int index;
 
 	for (index = 0; index < n; index++)
 	{
 		dest[index] = src[index];
 		n--;
 	}
+	return (dest);
 }
