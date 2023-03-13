7b7839c6b5f15895f8bc9d30144d9a076272f9b6
Chrisjulius <chrisogarjulius@gmail.com>
2023-03-13 11:35:47 -0700

    activity 3 done

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+  * _strspn - function to get the length of a prefix substring
+  * @s: string to search
+  * @accept: character to locate in string
+  * Return: returns the number of bytes of c found in s
+  */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int bytes = 0;
+	int i;
+
+	while (*s)
+	{
+		for (i = 0; accept[i]; i++)
+		{
+			if (*s == accept[i])
+				bytes++;
+			else if (accept[i + 1] == '\0')
+				return (bytes);
+		}
+		s++;
+	}
+	return (n);
+}
