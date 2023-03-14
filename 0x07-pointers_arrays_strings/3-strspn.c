2fd72dbe539c83727657559f551d2ac4d39338ec
Chrisjulius <chrisogarjulius@gmail.com>
2023-03-13 20:31:14 -0700

    added activity 3

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -15,7 +15,10 @@ unsigned int _strspn(char *s, char *accept)
 		for (i = 0; accept[i]; i++)
 		{
 			if (*s == accept[i])
+			{
 				bytes++;
+				break;
+			}
 			else if (accept[i + 1] == '\0')
 				return (bytes);
 		}
